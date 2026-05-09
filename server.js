cbb1ea7087a56921c0eac53e03d189b330bd70f8
tanawutmalirum29-ux <tanawutmalirum29@gmail.com>
2026-05-10 03:20:22 +0700

    server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const http = require("http");
+
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection",(socket)=>{
+
+    console.log("ผู้เล่นเชื่อมต่อ");
+
+});
+
+server.listen(process.env.PORT || 3000,()=>{
+
+    console.log("Server Running");
+
+});
